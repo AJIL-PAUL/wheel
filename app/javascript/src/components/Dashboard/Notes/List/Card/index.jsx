b62bf219788e1c6d3d9b71ccd8d1448be41a14dc
Ajil <ajilpaul.268@gmail.com>
2023-11-25 17:44:09 +0530

    Updated notes properties

--- app/javascript/src/components/Dashboard/Notes/List/Card/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/List/Card/index.jsx
@@ -5,15 +5,16 @@ import CardFooter from "./Footer";
 import CardHeader from "./Header";
 
 const Card = ({ note }) => {
-  const { title, description, categories, updatedAt, createdBy } = note;
+  const { title, description, tags, updatedAt, assignedContact, status } = note;
 
   return (
     <div className="neeto-ui-border-gray-300 neeto-ui-shadow-xs border p-4">
       <CardHeader title={title} />
       <CardBody description={description} />
       <CardFooter
-        categories={categories}
-        createdBy={createdBy}
+        assignedContact={assignedContact}
+        status={status}
+        tags={tags}
         updatedAt={updatedAt}
       />
     </div>
--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -1,3 +1,20 @@
+export const CONTACTS = [
+  { id: 1, name: "Oliver Smith", imageUrl: "https://i.pravatar.cc/300/1" },
+  { id: 2, name: "John Eve", imageUrl: "https://i.pravatar.cc/300/2" },
+  { id: 3, name: "Eve smith", imageUrl: "https://i.pravatar.cc/300/3" },
+];
+
+export const TAGS = [
+  { id: 1, label: "Shopping Cart" },
+  { id: 2, label: "Product Management" },
+  { id: 3, label: "User Guide" },
+];
+
+export const STATUS = {
+  CREATED: "Created",
+  DRAFTED: "Drafted",
+};
+
 export const INITIAL_NOTE_LIST = [
   {
     title: "How to Add Items to Your Shopping Cart",
@@ -12,48 +29,44 @@ export const INITIAL_NOTE_LIST = [
       your order. Enjoy the convenience of managing your shopping cart effortlessly and securely, making your
       online shopping experience a breeze.`,
     updatedAt: "2023-11-25T08:00:00Z",
-    categories: [{ id: 1, label: "Shopping Cart" }],
-    createdBy: {
-      name: "Oliver Smith",
-      imageUrl: "https://i.pravatar.cc/300/1",
-    },
+    tags: [TAGS[0]],
+    assignedContact: CONTACTS[0],
+    status: STATUS.CREATED,
   },
   {
     title: "Managing Your Shopping Cart Items",
     description:
       "Discover how to view, edit, or remove items from your shopping cart to streamline your shopping experience. ",
     updatedAt: "2023-11-24T10:30:00Z",
-    categories: [{ id: 2, label: "Product Management" }],
-    createdBy: { name: "John Eve", imageUrl: "https://i.pravatar.cc/300/2" },
+    tags: [TAGS[1]],
+    assignedContact: CONTACTS[1],
+    status: STATUS.DRAFTED,
   },
   {
     title: "Navigating Through Checkout Process",
     description:
       "Learn about the steps involved in the checkout process and how to complete your purchase securely.",
     updatedAt: "2023-11-23T15:45:00Z",
-    categories: [
-      { id: 1, label: "Shopping Cart" },
-      { id: 3, label: "User Guide" },
-    ],
-    createdBy: { name: "Eve smith", imageUrl: "https://i.pravatar.cc/300/3" },
+    tags: [TAGS[0], TAGS[2]],
+    assignedContact: CONTACTS[2],
+    status: STATUS.CREATED,
   },
   {
     title: "Applying Discount Codes",
     description:
       "Find out how to apply discount codes or coupons to your shopping cart to avail exciting offers and discounts.",
     updatedAt: "2023-11-22T09:20:00Z",
-    categories: [
-      { id: 1, label: "Shopping Cart" },
-      { id: 3, label: "User Guide" },
-    ],
-    createdBy: { name: "Sam John", imageUrl: "https://i.pravatar.cc/300/4" },
+    tags: [TAGS[0], TAGS[1]],
+    assignedContact: CONTACTS[2],
+    status: STATUS.DRAFTED,
   },
   {
     title: "Understanding Product Categories",
     description:
-      "Explore different product categories available on our platform to easily find the items you need.",
+      "Explore different product tags available on our platform to easily find the items you need.",
     updatedAt: "2023-11-21T14:00:00Z",
-    categories: [{ id: 2, label: "Product Management" }],
-    createdBy: { name: "John Simon", imageUrl: "https://i.pravatar.cc/300/5" },
+    tags: [TAGS[0]],
+    assignedContact: CONTACTS[0],
+    status: STATUS.CREATED,
   },
 ];
